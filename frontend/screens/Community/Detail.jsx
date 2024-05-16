6ba6e7d1dee9b7d3c756dc007d88433156038143
[gugudak] <[jdh4657@gmail.com]>
2024-05-16 16:59:59 +0900

    05.16

--- frontend/screens/Community/Detail.jsx
+++ frontend/screens/Community/Detail.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, Modal, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, Modal, TouchableOpacity, TextInput } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Ionicons를 import
 
 const Detail = ({ route }) => {
   const { title, category, keywords, content, purchase, trade, timestamp } = route.params;
   const [imageUri, setImageUri] = useState(null);
-  const [isImageModalVisible, setImageModalVisible] = useState(false); 
+  const [isImageModalVisible, setImageModalVisible] = useState(false);
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState(0); // State for keeping track of likes
 
- 
   const getImageUri = async () => {
-    
     const imageUri = route.params.image;
     setImageUri(imageUri);
   };
@@ -24,11 +26,26 @@ const Detail = ({ route }) => {
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    return `${year}.${month}.${day}  ${hours}:${minutes}`; // 공백
+    return `${year}.${month}.${day}  ${hours}:${minutes}`;
   };
 
   const toggleImageModal = () => {
     setImageModalVisible(!isImageModalVisible);
+  };
+
+  const handleCommentChange = (text) => {
+    setComment(text);
+  };
+
+  const handleAddComment = () => {
+    if (comment.trim() !== '') {
+      setComments([...comments, { text: comment, timestamp: new Date() }]);
+      setComment('');
+    }
+  };
+
+  const handleLike = () => {
+    setLikes(likes + 1);
   };
 
   return (
@@ -45,13 +62,14 @@ const Detail = ({ route }) => {
         <Text style={styles.text}>{content}</Text>
       </View>
       
-      {/* modal */}
+      {/* Image */}
       {imageUri && (
         <TouchableOpacity onPress={toggleImageModal}>
           <Image source={{ uri: imageUri }} style={styles.image} />
         </TouchableOpacity>
       )}
-      {/* modal */}
+      
+      {/* Image Modal */}
       <Modal visible={isImageModalVisible} transparent={true} onRequestClose={toggleImageModal}>
         <View style={styles.modalContainer}>
           <TouchableOpacity style={styles.closeButton} onPress={toggleImageModal}>
@@ -60,6 +78,41 @@ const Detail = ({ route }) => {
           <Image source={{ uri: imageUri }} style={styles.modalImage} />
         </View>
       </Modal>
+
+      {/* Comments */}
+      <View style={styles.commentContainer}>
+        <View style={styles.commentHeader}>
+          <Text style={styles.commentTitle}>💬</Text>
+          {/* Display the number of likes */}
+          <TouchableOpacity style={styles.likeButton} onPress={handleLike}>
+            <Ionicons name="heart" size={28} color="red" />
+            <Text style={styles.likeButtonText}>{likes}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.commentInputContainer}>
+          <TextInput
+            style={styles.commentInput}
+            placeholder="댓글을 입력하세요"
+            onChangeText={handleCommentChange}
+            value={comment}
+          />
+          <TouchableOpacity style={styles.commentButton} onPress={handleAddComment}>
+            <Ionicons name="arrow-forward-outline" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+        {/* 댓글 목록 */}
+        <View style={styles.commentsContainer}>
+            {comments.map((comment, index) => (
+              <View key={index} style={styles.commentContainer}>
+                <View style={styles.commentContent}>
+                  <Text style={styles.commentText}>{comment.text}</Text>
+                  <Text style={styles.commentTimestamp}>{formatTimestamp(comment.timestamp)}</Text>
+                </View>
+                {index !== comments.length - 1 && <View style={styles.separator}></View>}
+              </View>
+            ))}
+        </View>
+      </View>
     </View>
   );
 };
@@ -94,7 +147,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     marginBottom: 5,
   },
-  // 모달 스타일
+
   modalContainer: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
@@ -118,10 +171,65 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+  // Comments styles
+  commentContainer: {
+    marginTop: 10,
+  },
+  commentHeader: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginBottom: 10,
+  },
+  commentTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  likeButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 5, 
+  },
+  likeButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+  likeIcon: {
+    marginLeft: 5, // 아이콘과 숫자 사이의 간격 조정
+  },
+  commentInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  commentInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    padding: 10,
+    flex: 1,
+  },
+  commentButton: {
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  commentText: {
+    marginBottom: 5,
+  },
+  commentTimestamp: {
+    color: 'gray',
+    fontSize: 12,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'lightgray',
+    marginVertical: 5, // 수평 경계선의 상단 및 하단 간격
+  },
 });
 
 export default Detail;
-
-
-
-
